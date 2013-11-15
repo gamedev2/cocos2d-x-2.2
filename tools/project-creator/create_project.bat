bd4fdbd594855f3d82c80dd837fe224f2740a923
shines77 <gz_shines@msn.com>
2013-11-16 06:32:31 +0800

    Added the files of cocos2d-x version 2.2
    Signed-off-by: shines77 <gz_shines@msn.com>

---
+++ tools/project-creator/create_project.bat
@@ -0,0 +1,6 @@
+
+rem Usage: create_project.py -project <PROJECT_NAME> -package <PACKAGE_NAME> -language <cpp|lua|javascript>
+rem For exsample: create_project.py -package MyGame -k com.MyCompany.AwesomeGame -language javascript
+
+create_project.py -project WoPet -package com.i77soft.HoyStudio -language cpp
+pause
